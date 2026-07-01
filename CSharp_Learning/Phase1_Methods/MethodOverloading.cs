28e66daec7951bdc74d94a2290a1a6f44ac915dd
VaibhaviMasal <142891383+VaibhaviMasal@users.noreply.github.com>
2026-07-01 23:37:30 +0530

    MethodOverloading

---
+++ CSharp_Learning/Phase1_Methods/MethodOverloading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+namespace CSharp_Learning.Phase1_Methods
+{
+    internal class MethodOverloading
+    {
+        public static void Run()
+        {
+            int sum1 = Add(10, 20);
+            double sum2 = Add (20.5, 30.6);
+            int sum3 = Add(10, 20, 30);
+
+            Console.WriteLine($"Sum1 : {sum1}");
+            Console.WriteLine($"Sum2 : {sum2}");
+            Console.WriteLine($"Sum3 : {sum3}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add (double a, double b)
+        {
+            return a + b;
+        }
+
+        public static int Add (int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+    //Method Overloading -  Methos overloading is the process of creating multiple methods witht the same name but different parameters 
+}
--- CSharp_Learning/Program.cs
+++ CSharp_Learning/Program.cs
@@ -15,7 +15,8 @@ namespace CSharp_Learning
             //Conditionals.Run();
             //Loops.Run();
             //Methods.Run();
-            ReturnTypes.Run();
+            //ReturnTypes.Run();
+            MethodOverloading.Run();
         }
     }
 }
