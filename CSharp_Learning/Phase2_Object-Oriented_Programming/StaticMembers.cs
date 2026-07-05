083a278450a9893453f5a6582e3f8d12a662f052
VaibhaviMasal <142891383+VaibhaviMasal@users.noreply.github.com>
2026-07-05 19:46:25 +0530

    StaticMembers

---
+++ CSharp_Learning/Phase2_Object-Oriented_Programming/StaticMembers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Learning.Phase2_Object_Oriented_Programming
+{
+    // static :
+    // belongs to class.
+    // Access using ClassName.Member
+    // Only one copy exists.
+
+    // Class wheere only the variables are declared.
+    internal class BankAccount
+    {
+        public string accountHolder;
+        public static string bankName = "State Bank Of India";
+
+
+    }
+
+    // Main class here ; objects are created and called
+    internal class StaticMembers
+    {
+        public static void Run()
+        {
+            BankAccount account1 = new BankAccount();
+            BankAccount account2 = new BankAccount();
+
+            account1.accountHolder = "Vaibhavi";
+            account2.accountHolder = "Rahul";
+
+            Console.WriteLine(account1.accountHolder);
+            Console.WriteLine(account2.accountHolder);
+
+            Console.WriteLine(BankAccount.bankName);
+
+        }
+    }
+}
--- CSharp_Learning/Program.cs
+++ CSharp_Learning/Program.cs
@@ -21,7 +21,8 @@ namespace CSharp_Learning
             //Short_topics.Run();
             //ClassesObjects.Run();
             //Constructors.Run();
-            ThisKeyword.Run();
+            //ThisKeyword.Run();
+            StaticMembers.Run();
         }
     }
 }
